defea640a821ba5f9b28e21e6a9f8618140e9d87
Glorc <Moskva000miskva@gmail.com>
2025-12-10 01:46:11 +0300

    Попытка 2
    1

---
+++ Furniture/models/workshop.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class Workshop(db.Model):
+    __tablename__ = 'workshops'
+
+    workshop_id = db.Column(db.Integer, primary_key=True)
+    workshop_name = db.Column(db.String(255), nullable=False, unique=True)
+    workshop_type = db.Column(db.String(100), nullable=False)
+    staff_count = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class ProductWorkshop(db.Model):
+    __tablename__ = 'product_workshops'
+
+    product_workshop_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), nullable=False)
+    workshop_id = db.Column(db.Integer, db.ForeignKey('workshops.workshop_id'), nullable=False)
+    manufacturing_time_hours = db.Column(db.Numeric(8, 2), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relationship to Workshop
+    workshop = db.relationship("Workshop")
